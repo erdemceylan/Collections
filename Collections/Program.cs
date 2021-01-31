6aa54da85d0f52ceb19e1f14f8fbc791e5bf462b
erdemceylan <77546513+erdemceylan@users.noreply.github.com>
2021-01-31 18:00:48 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Erdem", "Mehmet", "Emre", "Ali" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            //isimler[4] = "Ahmet";
+            //Console.WriteLine(isimler[4]);
+            isimler = new string[5];
+            isimler[4] = "Burak";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+            List<string> isimler2 = new List<string> { "Erdem", "Mehmet", "Emre", "Ali" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Ahmet");
+            Console.WriteLine(isimler2[4]);
+
+
+        }
+    }
+}
